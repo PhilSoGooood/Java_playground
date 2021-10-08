435b55313409172e496e102fa0979bb620c36c41
PhilSoGooood <peelhw@gmail.com>
2021-10-08 13:33:01 +0900

    gugudan using class

--- gugudan/src/gugudan.java
+++ gugudan/src/gugudan.java
@@ -13,11 +13,4 @@ public class gugudan {
             System.out.println(result[i]);
         }
     }
-
-    public static void main(String[] args) {
-        for (int j = 2; j < 10; j++) {
-            int[] result = calculate(j);
-            print(result);
-        }
-    }
 }
---
+++ gugudan/src/gugudanMain.java
@@ -0,0 +1,7 @@
+public class gugudanMain {
+    public static void main(String[] args) {
+        for (int j = 2; j < 10; j++) {
+            gugudan.print(gugudan.calculate(j));
+        }
+    }
+}
